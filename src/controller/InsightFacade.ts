69f7897540e3f1b9563d8f211cb529ee0ba668f3
treysfordays <austinkwok@live.ca>
2017-01-27 13:48:47 -0800

    Add InsightFacade.ts

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -4,15 +4,28 @@
 import {IInsightFacade, InsightResponse, QueryRequest} from "./IInsightFacade";
 
 import Log from "../Util";
+var dataStructure: any = {};
 
 export default class InsightFacade implements IInsightFacade {
+
+
 
     constructor() {
         Log.trace('InsightFacadeImpl::init()');
     }
 
     addDataset(id: string, content: string): Promise<InsightResponse> {
-        return null;
+        return new Promise (function (fulfill, reject) {
+            var JSZip = require("jszip");
+            dataStructure[id]={};
+
+            JSZip.loadAsync(content, {base64: true}).then(function(zipFile: any){
+                console.log(zipFile.files);
+                Object.keys(zipFile.files).forEach(function (key: any) {
+                    //console.log(key);
+                });
+            })
+        });
     }
 
     removeDataset(id: string): Promise<InsightResponse> {
@@ -20,6 +33,7 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     performQuery(query: QueryRequest): Promise <InsightResponse> {
+        
         return null;
     }
 }

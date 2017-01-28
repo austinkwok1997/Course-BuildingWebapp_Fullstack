d4f29ada33ef7184be0bea1afa112ccd39c88682
treysfordays <austinkwok@live.ca>
2017-01-27 22:14:11 -0800

    sending current code with a TODO for partner

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -19,12 +19,31 @@ export default class InsightFacade implements IInsightFacade {
             var JSZip = require("jszip");
             dataStructure[id]={};
 
+            // TODO: figue out how to get to every file and read it's content
+            // once you are able to figure it out, message me and I'll be able to put
+            // the ontent in the dataStructure
+
+            // loads the data
             JSZip.loadAsync(content, {base64: true}).then(function(zipFile: any){
-                console.log(zipFile.files);
+
+                // grabbing the keys in zipFile.files
+                console.log("in load Async");
                 Object.keys(zipFile.files).forEach(function (key: any) {
-                    //console.log(key);
+                    // I believe the problem is here, because of the async
+
+                    console.log("in the keys function");
+                    zipFile.files[key].async('string').then(function (fileData : any) {
+                        // this is supposed to grab the contenet in each file and turn it into a JSON file
+                        // PLEASE HELP ME FIX THIS
+                        console.log("inside the async fucntion");
+                        var obj = JSON.parse(fileData);
+                        console.log(obj);
+
+                    });
+                    console.log("outside async function");
                 });
-            })
+            });
+            console.log("outside iterating through keys");
         });
     }
 

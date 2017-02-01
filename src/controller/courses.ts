450809a168cde63ef995c98d73b270d983768416
treysfordays <austinkwok@live.ca>
2017-02-01 13:37:26 -0800

    created a course object

---
+++ src/controller/courses.ts
@@ -0,0 +1,88 @@
+/**
+ * Created by Austin on 2017-02-01.
+ */
+export class courses {
+    courses_dept: string;
+    courses_id: string;
+    courses_avg: number;
+    courses_instructor: string;
+    courses_title: string;
+    courses_pass: number;
+    courses_fail: number;
+    courses_audit: number
+    courses_uuid: string;
+    file_id: string;
+    constructor(id: string){
+        this.courses_dept = null;
+        this.courses_id = null;
+        this.courses_avg = null;
+        this.courses_instructor = null;
+        this.courses_title = null;
+        this.courses_pass = null;
+        this.courses_fail = null;
+        this.courses_audit = null;
+        this.courses_uuid = null;
+        this.file_id = id;
+    }
+
+    getDept(){
+        return this.courses_dept;
+    }
+    getCourseId(){
+        return this.courses_id;
+    }
+    getAvg(){
+        return this.courses_avg;
+    }
+    getInstructor(){
+        return this.courses_instructor;
+    }
+    getTitle(){
+        return this.courses_title;
+    }
+    getPass(){
+        return this.courses_pass;
+    }
+    getFail(){
+        return this.courses_fail;
+    }
+    getAudit(){
+        return this.courses_audit;
+    }
+    getUuid(){
+        return this.courses_uuid;
+    }
+    getFileId(){
+        return this.file_id;
+    }
+    setDept(dept: string){
+        this.courses_dept = dept;
+    }
+    setCourseId(courseId: string){
+        this.courses_id = courseId;
+    }
+    setAvg(avg: number){
+        this.courses_avg = avg;
+    }
+    setInstructor(instructor: string){
+        this.courses_instructor = instructor;
+    }
+    setTitle(title: string){
+        this.courses_title = title;
+    }
+    setPass(pass: number){
+        this.courses_pass = pass;
+    }
+    setFail(fail: number){
+        this.courses_fail = fail;
+    }
+    setAudit(audit: number){
+        this.courses_audit = audit;
+    }
+    setUuid(uuid: string){
+        this.courses_uuid = uuid;
+    }
+    setFileId(fileId: string){
+        this.file_id = fileId;
+    }
+}

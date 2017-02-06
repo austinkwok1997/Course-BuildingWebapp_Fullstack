f862ae061641c279f3d278ee735afcd2e2d8c77d
treysfordays <austinkwok@live.ca>
2017-02-05 23:30:45 -0800

    Update InsightFacadeSpec.ts

--- test/InsightFacadeSpec.ts
+++ test/InsightFacadeSpec.ts
@@ -78,17 +78,71 @@ describe("InsightFacadeSpec", function() {
         });
     })
 
-    /*
-    it("addDataset test", function() {
-        zipContent = fs.readFileSync("courses.zip").toString("base64");
-        return facade.addDataset("courses", zipContent).then(function(InF:InsightResponse){
-
+    it ('empty zip file', function() {
+        zipContent = fs.readFileSync("empty.zip").toString("base64");
+        return facade.addDataset("empty", zipContent).then(function (InF: InsightResponse) {
             console.log(InF.code+": "+JSON.stringify(InF.body));
-        }).catch(function(err:any){
+        }).catch(function(err:any) {
             console.log(err);
             expect.fail();
         });
-
     });
-*/
+
+    it ('zipfile with empty JSON', function() {
+        zipContent = fs.readFileSync("emptyJSON.zip").toString("base64");
+        return facade.addDataset("emptyJSOM", zipContent).then(function (InF: InsightResponse) {
+            console.log(InF.code+": "+JSON.stringify(InF.body));
+        }).catch(function(err:any) {
+            console.log(err);
+            expect.fail();
+        });
+    });
+
+    it ('zipfile with empty Array', function() {
+        zipContent = fs.readFileSync("emptyArray.zip").toString("base64");
+        return facade.addDataset("emptyArray", zipContent).then(function (InF: InsightResponse) {
+            console.log(InF.code+": "+JSON.stringify(InF.body));
+        }).catch(function(err:any) {
+            console.log(err);
+            expect.fail();
+        });
+    });
+
+    it ('zipfile with picture', function() {
+        zipContent = fs.readFileSync("Picture.zip").toString("base64");
+        return facade.addDataset("Picture", zipContent).then(function (InF: InsightResponse) {
+            console.log(InF.code+": "+JSON.stringify(InF.body));
+            expect.fail();
+        }).catch(function(err:any) {
+            console.log(err);
+        });
+    });
+
+    it ('Testing with null', function() {
+        zipContent = fs.readFileSync("Picture.zip").toString("base64");
+        return facade.addDataset("Picture", null).then(function (InF: InsightResponse) {
+            console.log(InF.code+": "+JSON.stringify(InF.body));
+            expect.fail();
+        }).catch(function(err:any) {
+            console.log(err);
+        });
+    });
+    it ('Testing with undefined', function() {
+        zipContent = fs.readFileSync("Picture.zip").toString("base64");
+        return facade.addDataset("Picture", undefined).then(function (InF: InsightResponse) {
+            console.log(InF.code+": "+JSON.stringify(InF.body));
+            expect.fail();
+        }).catch(function(err:any) {
+            console.log(err);
+        });
+    });
+    it ('Testing with empty string', function() {
+        zipContent = fs.readFileSync("Picture.zip").toString("base64");
+        return facade.addDataset("Picture", "").then(function (InF: InsightResponse) {
+            console.log(InF.code+": "+JSON.stringify(InF.body));
+            expect.fail();
+        }).catch(function(err:any) {
+            console.log(err);
+        });
+    });
 });

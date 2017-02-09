c52023a1c7b0b27443cfb0f8221559024ec30d85
jjilly <jackerjilly@live.com>
2017-02-09 13:37:58 -0800

    rejcet(null) added to tadddataset

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -111,6 +111,10 @@ export default class InsightFacade implements IInsightFacade {
                     });
 
                     console.log("#0.1; outside iterating through id: " + id);
+            
+     //TODO
+reject(null);
+return;
                 });
             } else {
                 let alreadyHasInsResp: InsightResponse = {

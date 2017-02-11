cc226a0f640a95dfdb2ae2536b90ba488f85a5fa
jjilly <jackerjilly@live.com>
2017-02-11 01:43:06 -0800

    (5.1.3.0.1)

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -236,7 +236,7 @@ export default class InsightFacade implements IInsightFacade {
                                    // if (underscoreWord === 'id') {
                                     //    resultObject[underscoreWord] = courseTermData[that.keyToJsonKey(that.underscoreManager(underscoreWord,'key'))].toString; //special case if keyArray element is id we need to turn the int into a string
                                   //  } else {
-                                   //     resultObject[underscoreWord] = courseTermData[that.keyToJsonKey(that.underscoreManager(underscoreWord,'key'))];//take desired keys from result object and fill them with the values in valid courseTermData
+                                        resultObject[underscoreWord] = courseTermData[that.keyToJsonKey(that.underscoreManager(underscoreWord,'key'))];//take desired keys from result object and fill them with the values in valid courseTermData
                                    // }
 
 
